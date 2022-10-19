5cac6112a70ff0a0ca25a65db16e1183e6ea2d5d
SMFahimShahrear <s.m.shahrear.fahim@gamil.com>
2022-10-19 12:57:27 +0600

    logic added

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let op ='';
 let x = 0;
 let y = 1;
 const memory = [];
+const temp = [];
 let flag = 0;
 let op_flag = 0;
 let i = 0;
@@ -20,9 +21,54 @@ function btn_call(e) {
     screen_1.innerHTML = disp;
 
     if (e == "+" || e == "-" || e == "x" || e == "/" || e == "=") {
-
-        if(e == "+"){
-            
+        op = e;
+        for(j=0; j<memory[memory.length-1]; j++){
+            if(memory.length == 1){
+                op_flag = 1;
+                break;
+            }
+            else if(memory[j] == "/"){
+                num = memory[j-1] / memory[j+1];
+                memory.splice(j-1, 5 , num, "/");
+                i -=2;
+                console.log("/:", memory)
+            }
+        }
+        for(j=0; j<memory[memory.length-1]; j++){
+            if(memory.length == 1){
+                op_flag = 1;
+                break;
+            }
+            else if(memory[j] == "x"){
+                num = memory[j-1] * memory[j+1];
+                memory.splice(j-1, 5 , num, "x");
+                i -=2;
+                console.log("x:", memory)
+            }
+        }
+        for(j=0; j<memory[memory.length-1]; j++){
+            if(memory.length == 1){
+                op_flag = 1;
+                break;
+            }
+            else if(memory[j] == "-"){
+                num = memory[j-1] - memory[j+1];
+                memory.splice(j-1, 5 , num, "-");
+                i -=2;
+                console.log("-:", memory)
+            }
+        }
+        for(j=0; j<memory[memory.length-1]; j++){
+            if(memory.length == 1){
+                op_flag = 1;
+                break;
+            }
+            else if(memory[j] == "+"){
+                num = memory[j-1] + memory[j+1];
+                memory.splice(j-1, 5 , num, "+");
+                i -=2;
+                console.log("+:", memory)
+            }
         }
 
         i++;
@@ -32,8 +78,22 @@ function btn_call(e) {
     }
     else{
         memory[i]= parseFloat(init);
+        if(op_flag == 1){
+            x= parseFloat(init);
+        }
+        if(op == "/"){
+            screen_2.innerText = parseFloat(memory[0]) / x;
+        }
+        else if(op == "x"){
+            screen_2.innerText = parseFloat(memory[0]) * x;
+        }
+        else if(op == "-"){
+            screen_2.innerText = parseFloat(memory[0]) - x;
+        }
+        else if(op == "+"){
+            screen_2.innerText = parseFloat(memory[0]) + x;
+        }
     }
-    // console.log(memory[memory.length-1])
 }
 
 
